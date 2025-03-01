991abe4f518bbb40c4df228a45778ab750027829
awarsha2 <awarsha2@gmail.com>
2025-03-01 12:51:32 -0500

    base stuff added, no functionality for youtube yet

---
+++ do-stuff.js
@@ -0,0 +1,7 @@
+const myButton = document.getElementById("myButton");
+  myButton.addEventListener("click", function() {
+    console.log("auto-notify-me opperational!")
+    alert("Button clicked!");
+    // add your JavaScript code here
+    console.log("auto-notify-me shutting down...")
+  });
